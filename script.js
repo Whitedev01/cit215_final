e70d249272c0400dc3241a6cfc21c0d64883bf0a
Whitedev01 <55120552+Whitedev01@users.noreply.github.com>
2020-11-20 20:57:02 -0500

    commit 1

---
+++ script.js
@@ -0,0 +1,65 @@
+new Vue({
+  el: "#flashcards-app",
+  data: {
+    show:false,
+    cartImage:"https://www.clipartmax.com/png/middle/270-2708201_download-shopping-cart-icon-cart-icon-hd.png",
+    cards: [
+      {
+        name: "Tuba",
+        type: "Brass" ,
+        price:"3,839.99",
+        brand: "Yamaha",
+        image:"https://images-na.ssl-images-amazon.com/images/I/6122UYjgW5L._AC_SL1000_.jpg",
+        color:"green",
+        
+      },
+    {
+        name: "Saxophone",
+        type: "Brass" ,
+        price:"3,443.99",
+        brand: "Yamaha",
+        image:"https://images.samash.com/sa/YAS/YAS480XXX-P.fpx?cvt=jpg",
+        color:"green",
+        
+      },
+      {
+        name: "Brad Esquire",
+        type: "Electric Guitar" ,
+        price:"3,839.99",
+        brand: "Fender",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVyOZ_B7k_nav1yM-K0nzDSmQ6SNJHG6K0Mg&usqp=CAU",
+        color:"red",
+        
+      },
+      {
+        name: "American Bass",
+        type: "Guitar Bass" ,
+        price:"1,599.99",
+        brand: "Fender",
+        image:"https://cdn.shopify.com/s/files/1/0856/7056/products/FenderAmericanProfessionalIIJazzBassMPDarkNight_710x.png?v=1600708392",
+        color:"red",
+        
+      },
+      {
+        name: "S60",
+        type: "Guitar" ,
+        price:"1,244.99",
+        brand: "Maton",
+        image:"https://maton.com.au/images/made/assets/uploads/products/S60_Front_900_422_s.png",
+        color:"blue",
+        
+      },
+      {
+        name: "S70",
+        type: "Guitar" ,
+        price:"1,499.99",
+        brand: "Maton",
+        image:"https://zenithmusic.com.au/wp-content/uploads/2018/12/S70-THUMBNAIL.jpg",
+        color:"blue",
+      }
+    ]
+  },
+  medthod:{
+    
+  }
+})
